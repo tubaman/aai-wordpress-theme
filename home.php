eaa8ad4ba342fee88cca5e5706fcc6cbbdbe09ef
Ryan Nowakowski <rnowakow@qualcomm.com>
2012-03-27 23:45:40 -0500

    get rid of recent articles

--- home.php
+++ home.php
@@ -17,32 +17,6 @@
 
 
 <div id="main-content" class="clearfix">
-	<div id="left-area">
-		<h4 class="main-title"><?php _e('Most Recent Articles','Aggregate'); ?></h4>
-		<div id="entries">
-			<?php
-				$args=array(
-					'showposts'=>get_option('aggregate_homepage_posts'),
-					'paged'=>$paged,
-					'category__not_in' => get_option('aggregate_exlcats_recent'),
-				);
-				if (get_option('aggregate_duplicate') == 'false') $args['post__not_in'] = $ids;
-				query_posts($args);
-				global $paged;
-				$i = 0;
-			?>
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				<?php include(TEMPLATEPATH . '/includes/entry.php'); ?>
-			<?php endwhile; ?>
-				<?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); }
-				else { ?>
-					 <?php include(TEMPLATEPATH . '/includes/navigation.php'); ?>
-				<?php } ?>
-			<?php else : ?>
-				<?php include(TEMPLATEPATH . '/includes/no-results.php'); ?>
-			<?php endif; wp_reset_query(); ?>
-		</div> <!-- end #entries -->
-	</div> <!-- end #left-area -->
 	
 	<?php if ( is_active_sidebar( 'homepage-sidebar' ) ) { ?>
 		<div id="sidebar">
